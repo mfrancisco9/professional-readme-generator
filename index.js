d2200ed6271c0581cdd4abbcfc42a016594e9b87
Michael Francisco <m.francisco9@gmail.com>
2021-04-21 21:53:43 -0500

    started switch function for license tags

--- index.js
+++ index.js
@@ -54,8 +54,44 @@ const questions = [
     }
     ];
 
+
+function getLicenseTags(){
+let licenseTag = ""
+let license = (response.license);
+switch (license) {
+    case 'MIT':
+        licenseTag = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        break;
+    case 'GPLv2':
+        licenseTag ="[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+        break;
+    case 'Apache':
+        licenseTag ="[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        break;
+    case 'GPLv3':
+        licenseTag ="[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+        break;
+    case 'BSD 3-clause':
+        licenseTag ="[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+        break;
+    case 'Unlicense':
+        licenseTag ="[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+        break;
+    case 'BSD 2-clause':
+        licenseTag ="[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+        break;
+    case 'LGPLv3':
+        licenseTag ="[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+        break;
+    case 'AGPLv3':
+        licenseTag ="[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+        break;
+}
+return licenseTag;
+};
+
 function generateReadMe(response){
-    let template = `ok`
+    let template = ``
     return template;
 };
 
--- template.md
+++ template.md
@@ -1,15 +1,4 @@
-#${response.projectName}
-
-Table of Contents |
-------------------|
-Description|
-Installation|
-Usage|
-Contributing|
-Tests|
-License|
-Questions|
-
+# ${response.projectName}
 
 # Table of contents
 1. [Description](#description)
@@ -21,16 +10,22 @@ Questions|
 7. [Questions](#questions)
 
 ## Description: <a name="description"></a>
+${response.description}
 
 ## Installation <a name="installation"></a>
+${response.installation}
 
 ## Usage <a name="usage"></a>
+${response.usage}
 
 ## Contributing <a name="contributing"></a>
+${response.contributing}
 
 ## Tests <a name="tests"></a>
+${response.tests}
 
 # License <a name="license></a>
+${response.license}
 
 # Questions <a name="questions">
 For any questions relating to this project, please contact me via [Github](http://github.com/${response.username}) or email me directly at [${response.email}](${response.email}).
